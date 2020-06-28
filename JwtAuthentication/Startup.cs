3c11f8cfb024263324c27e2fa23768f6eac2faec
Rashik Tuladhar <rashiktuladhar2016@outlook.com>
2020-06-28 10:26:32 +0545

    swagger added with authorization for bearer token

--- JwtAuthentication/Startup.cs
+++ JwtAuthentication/Startup.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 namespace JwtAuthentication
 {
     public class Startup
@@ -37,6 +42,56 @@ namespace JwtAuthentication
                     };
                 });
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new OpenApiInfo
+                {
+                    Title = "Swagger Implementation",
+                    Version = "V1",
+                    Description = "Api Description",
+                    TermsOfService = new Uri("https://rashik.com.np"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Company Name",
+                        Email = "info@email.com",
+                        Url = new Uri("https://rashik.com.np"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Swagger Implementation License",
+                        Url = new Uri("https://rashik.com.np"),
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +101,13 @@ namespace JwtAuthentication
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/V1/swagger.json", "Integrating Swagger");
+            });
+
             app.UseAuthentication();
             app.UseHttpsRedirection();
 
